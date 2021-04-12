e23f5d9bb902a9ec1c2a7cd665fdeb97e17cae24
Andrey Krivosheev <feuiuxbe@gmail.com>
2021-04-12 13:36:19 +0200

    add: update 03 task
    some linter tests are failing

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,26 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+
+function sortByHeight(arr) {
+  const returnarr = [];
+  let min = Infinity;
+  let minindex;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      returnarr[i] = arr[i];
+    } else {
+      for (let k = i; k < arr.length; k++) {
+        if (arr[k] < min && arr[k] !== -1) {
+          min = arr[k];
+          minindex = k;
+        }
+      }
+      returnarr[i] = min;
+      arr.splice[minindex];
+    }
+  }
+  return returnarr;
 }
 
 module.exports = sortByHeight;
